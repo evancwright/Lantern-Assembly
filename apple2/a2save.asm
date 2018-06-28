6a934efce4250710f7941ab9508fd62bf61a4246
Evan C. Wright <evancwright@yahoo.com>
2018-06-27 22:35:28 -0500

    can load and save now

--- apple2/a2save.asm
+++ apple2/a2save.asm
@@ -1,20 +1,31 @@
 ;save/restore routines for Apple2
 #define PRODOS $BF00
 
-; $C0  =  create a new file  
-; $C1  =  destroy an existing file
-; $C4  =  get file info (a dummy command, do not use)
-; $C8  =  open a file (reference numbers 0, 1, or 2)
-; $CA  =  read from a file
-; $CB  =  write to a file
-; $CC  =  close a file
-; $CE  =  position file marker
-save
+#define MLI  $BF00 ; PRODOS MACHINE LANGUAGE INTERFACE
+#define FCREATE $C0
+#define FDESTROY $C1
+#define FGETINFO $C4
+#define FOPEN $C8
+#define FCLOSE $CC
+#define FWRITE $CB
+#define FREAD $CA
+#define SET_PREFIX $C6
+#define FILE_NOT_FOUND $46
+#define TEXT_FILE $04
+#define BINARY_FILE $06
+#define NORMAL_ACCESS $C3
+#define STANDARD_FILE $01
+
+save_sub
+	lda #0
+	sta ioErrFlg
 	lda #saving%256 ; print the prompt
 	sta strAddr
 	lda #saving/256
 	sta strAddr+1
 	jsr printstrcr
+	jsr destroy_file
+	jsr create_file
     jsr open_file
 	jsr write_file
 	jsr close_file
@@ -25,6 +36,47 @@ save
 	jsr printstrcr
 	rts
 
+	.module restore_sub
+restore_sub
+	lda #restoring%256 ; print the prompt
+	sta strAddr
+	lda #restoring/256
+	sta strAddr+1
+	jsr printstrcr
+	jsr open_file
+	jsr read_file
+	jsr close_file
+	jsr look_sub
+	rts	
+
+	
+	
+;destroys the file name whose
+;name is in the buffer.
+;called as part of recreating a file
+;when it is written to.
+;errors are not checked for. An error
+;will occur on the first save trying to
+;destroy a file that doesn't exist
+destroy_file	
+	jsr MLI
+	.db FDESTROY
+	.dw destroyBlock 
+	rts
+	
+	.module	create_file	
+create_file	
+	jsr MLI
+	.db FCREATE
+	.dw createParams
+	bcs _ferr
+	rts
+_ferr	
+	sta ioErrFlg
+	jsr print_io_err
+	rts	
+	
+	
 ;** Open an existing file   
 ;command number $C8
 ;parameters:     
@@ -32,54 +84,179 @@ save
 ; +1     (pointer to pathname)           _required_
 ; +3     (pointer to i/o buffer)         _required_**
 ; +5     (reference number, 0, 1, 2)     _required_ (result)
+;opens a file for reading/writing
+	.module open_file
 open_file
-	lda 3
-	sta numparams
-	lda #filename%256 ; file path to open
-	sta $params+1
-	lda #filename/256
-	sta $params+2
-	lda #diskbuf%256	; io buffer
-	sta $params+3
-	lda #diskbuf/256
-	sta $params+4
-	lda #0
-	sta $params+5   ; file ref num
-	jsr PRODOS
-	.byte $C8 	 ; ProDOS command number = C8 (read)
-    .word params ; address of parameter table, lo/hi
+	jsr MLI
+	.db FOPEN
+	.dw openParams 
+	bcs _ferr
+	rts
+_ferr
+	sta ioErrFlg
+	jsr print_open_err
 	rts
 
 ; write data
 write_file
-	lda #$04
-    sta numparams	;store number of params
-	lda #beginData%256 ;store the buffer
-	sta $params+1
-	lda #beginData/256
-	sta $params+2
-	sec ; subtract end from start
-	lda #endData%256
-	sbc #beginData%256
-	sta params+3
-	lda #endData/256
-	sbc #beginData/256
-	sta params+4	
+	lda fRef ; set fileRef
+	sta writeParams+1
 	jsr PRODOS
-	.byte $C8 	 ; ProDOS command number = C8 (read)
-    .word params ; address of parameter table, lo/hi	
+	.byte FWRITE 	 ; ProDOS command number = C8 (read)
+    .word writeParams ; address of parameter table, lo/hi	
+	bcs _err
+	rts
+_err
+	sta ioErrFlg
+	jsr print_io_err
 	rts
 	
-; closes the open file
-close_file
-	lda 1
-	sta numparams
-	lda fileref
-	sta params+1
+ 
+; write data
+	.module read_file
+read_file
+	lda fRef ; set fileRef
+	sta writeParams+1
 	jsr PRODOS
-	.byte $CC
-	.word params ; address of parameter table, lo/hi
-    rts 
+	.byte FREAD 	 ; ProDOS command number = C8 (read)
+    .word writeParams ; address of parameter table, lo/hi	
+	bcs _err
+	rts
+_err
+	sta ioErrFlg
+	jsr print_io_err
+	rts
+	
+	.module close_file
+close_file
+	lda fRef
+	sta closeRef
+	jsr MLI
+	.db FCLOSE
+	.dw closeParams
+	bcs _ferr
+	rts
+_ferr
+	sta ioErrFlg
+	jsr print_close_err
+	rts
+
+	
+print_io_err
+	lda #ioErrMsg%256
+	sta $strAddr
+	lda #ioErrMsg/256
+	sta $strAddr+1		
+	jsr printstrcr
+	rts
+
+print_open_err
+	lda #opnErrMsg%256
+	sta $strAddr
+	lda #opnErrMsg/256
+	sta $strAddr+1		
+	jsr printstrcr
+	rts
+	
+print_fnf_err
+	lda #fnfMsg%256
+	sta $strAddr
+	lda #fnfMsg/256
+	sta $strAddr+1		
+	jsr printstrcr
+	rts	
+
+print_close_err
+	lda #closeErrMsg%256
+	sta $strAddr
+	lda #closeErrMsg/256
+	sta $strAddr+1		
+	jsr printstrcr
+	rts
+	
+destroyBlock
+	.db 1
+	.dw nameLen
+	
+;    7   6   5   4   3   2   1   0
+;    +---+---+---+---+---+---+---+---+
+;  0 | param_count = 7               |
+;    +---+---+---+---+---+---+---+---+
+;  1 | pathname               (low)  |
+;  2 | (2-byte pointer)       (high) |
+;    +---+---+---+---+---+---+---+---+
+;  3 | access         (1-byte value) |
+;    +---+---+---+---+---+---+---+---+
+;  4 | file_type      (1-byte value) |
+;    +---+---+---+---+---+---+---+---+
+;  5 | aux_type               (low)  |
+;  6 | (2-byte value)         (high) |
+;    +---+---+---+---+---+---+---+---+
+;  7 | storage_type   (1-byte value) |
+;    +---+---+---+---+---+---+---+---+
+;  8 | create_date          (byte 0) |
+;  9 | (2-byte value)       (byte 1) |
+;    +---+---+---+---+---+---+---+---+
+;  A | create_time          (byte 0) |
+;  B | (2-byte value)       (byte 1) |
+;    +---+---+---+---+---+---+---+---+	
+createParams
+	.db 7
+	.dw nameLen  ; len + buffer
+	.db NORMAL_ACCESS
+	.db BINARY_FILE
+	.dw 0  ; aux type?
+	.db STANDARD_FILE
+	.dw 0  ; create date
+	.dw 0  ; create time
+
+;   7   6   5   4   3   2   1   0
+;    +---+---+---+---+---+---+---+---+
+;  0 | param_count = 3               |
+;    +---+---+---+---+---+---+---+---+
+;  1 | pathname               (low)  |
+;  2 | (2-byte pointer)       (high) |
+;    +---+---+---+---+---+---+---+---+
+;  3 | io_buffer              (low)  |
+;  4 | (2-byte pointer)       (high) |
+;    +---+---+---+---+---+---+---+---+
+;  5 | ref_num       (1-byte result) |
+;    +---+---+---+---+---+---+---+---+	
+openParams
+	 .db 3  ; param count
+	 .dw nameLen
+	 .db 00,9E  ; buffer
+fRef .db 0 ; fileRef
+	
+
+closeParams
+	.db 1   ; param count
+closeRef
+	.db 0
+
+; 7   6   5   4   3   2   1   0
+;    +---+---+---+---+---+---+---+---+
+;  0 | param_count = 4               |
+;    +---+---+---+---+---+---+---+---+
+;  1 | ref_num        (1-byte value) |
+;    +---+---+---+---+---+---+---+---+
+;  2 | data_buffer            (low)  |
+;  3 | (2-byte pointer)       (high) |
+;    +---+---+---+---+---+---+---+---+
+;  4 | request_count          (low)  |
+;  5 | (2-byte value)         (high) |
+;    +---+---+---+---+---+---+---+---+
+;  6 | trans_count            (low)  |
+;  7 | (2-byte result)        (high) |
+;    +---+---+---+---+---+---+---+---+
+writeParams
+	.db 4
+	.db 0 ; fileRef (filled in by write call)
+	.dw beginData
+	.dw endData-beginData
+	.dw 0
+	
+ 
   	
 fnf	.text "FILE NOT FOUND."
 	.byte 0		
@@ -87,15 +264,22 @@ selslot	.text "SELECT A SLOT[1-5]."
 	.byte 0		
 saving	.text "SAVING..."
 	.byte 0		
- 
-params  
-numparams 		.byte 0 ; 
-				.byte 0 ;
-				.byte 0 ;
-				.byte 0 ;
-fileref			.byte 0 ; filled in by ProDos
+restoring	.text "RESTORING..."
+	.byte 0  
 	
 diskbuf .byte 0 ; just read one byte at a time	
-
-filename .text "SAVE0.SAV"
-	.byte 0 
+	
+fnfMsg .text "FILE NOT FOUND."
+			.byte 0			
+ioErrMsg .text "I/O ERR."
+			.byte 0	
+opnErrMsg .text "OPEN ERR."
+			.byte 0	
+closeErrMsg .text "CLOSE ERR."
+			.byte 0				
+ioErrFlg .byte 0 
+			
+nameLen .db 13
+nameBuf .text "/SYSTEM/SAVE0"			
+nameEnd
+			 	
