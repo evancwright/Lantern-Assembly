8634a5b0b9838692724fa0454e554770329d68f9
Evan C. Wright <evancwright@yahoo.com>
2019-09-17 06:07:46 +0100

    build script fixes

--- c64Merlin/build.bat
+++ c64Merlin/build.bat
@@ -6,9 +6,9 @@ if EXIST advent.d64 (
 )
 
 echo Assembling...
-.\tasm -65 -b -c -s main.asm advent.prg
+..\bin\Merlin32.exe  -V ..\Merlin32_v1.0\Library main.s
 
-copy blank.d64 advent.d64
+copy blank.d64 __DISK_NAME__.d64
  
 echo Attaching program to disk image...
 ..\bin\c1541 -attach advent.d64 -write advent.prg advent
@@ -16,3 +16,7 @@ echo Attaching program to disk image...
 echo Program is ready to run
 echo Reminder:
 echo LOAD "$",8 loads the directory
+echo LOAD "advent.prg",8 loads the program
+
+echo "attaching program to disk image..."
+..\bin\c1541 -attach __DISK_NAME__.d64 -write main advent.prg
