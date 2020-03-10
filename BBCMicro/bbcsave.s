3bf406c451bfdf51870f248a79b329baeda01438
Evan C. Wright <evancwright@yahoo.com>
2020-03-10 18:26:21 +0100

    added not impl messages

--- BBCMicro/bbcsave.s
+++ BBCMicro/bbcsave.s
@@ -1,5 +1,18 @@
 save_sub
+	lda #<notimpl
+	sta strAddr
+	lda #>notimpl
+	sta strAddr+1
+	jsr printstrcr
    rts
    
 restore_sub
+	lda #<notimpl
+	sta strAddr
+	lda #>notimpl
+	sta strAddr+1
+	jsr printstrcr
    rts
+
+notimpl ASC 'Not implemented.'
+	DB 0
